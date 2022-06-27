fe1631c61ba56f11c6c8705538c6d86e865cfa8d
Juan Moreno <jmoreno@griddynamics.com>
2022-06-27 10:22:35 -0500

    First commit

---
+++ src/test/java/org/example/BookSpring/BookSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.BookSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
