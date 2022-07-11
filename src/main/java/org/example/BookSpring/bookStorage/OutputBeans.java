794e772157bcffaa365fec6a420fcbd7ceefb41c
Juan Moreno <jmoreno@griddynamics.com>
2022-07-11 12:50:06 -0500

    Show all bean names in the context

---
+++ src/main/java/org/example/BookSpring/bookStorage/OutputBeans.java
@@ -0,0 +1,24 @@
+package org.example.BookSpring.bookStorage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+@Configuration
+public class OutputBeans {
+    Logger logger = Logger.getLogger(OutputBeans.class.getName());
+
+    @Autowired
+    public void showBeans(ApplicationContext ctx) {
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            logger.info(beanName);
+        }
+    }
+
+}
