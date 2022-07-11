241ad73e489ea16243f9227c36602b1d1366e385
Juan Moreno <jmoreno@griddynamics.com>
2022-07-11 12:19:45 -0500

    Fix the way to subscribe subscribers

--- src/main/java/org/example/BookSpring/bookStorage/Notification/ManageSubscriber.java
+++ src/main/java/org/example/BookSpring/bookStorage/Notification/ManageSubscriber.java
@@ -1,47 +1,31 @@
 package org.example.BookSpring.bookStorage.Notification;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
 
-//EventManager
 @Component
-public class ManageSubscriber implements PublisherI{
+public class ManageSubscriber implements PublisherI {
+    private final Map<String, List<Observer>> eventSub = new HashMap<>();
 
-   private final Map<String, List<Observer>> eventSub;
-    private final SubscriberConfig subscriberConfig;
-
-    @Autowired
-    public ManageSubscriber(SubscriberConfig subscriberConfig) {
-
-        this.subscriberConfig = subscriberConfig;
-        this.eventSub = subscriberConfig.allEvents();
-    }
-
-    public void subscribe(String eventType, Observer newSub){
+    public void subscribe(String eventType, Observer newSub) {
 
         Optional.ofNullable(eventSub.get(eventType))
-                .ifPresent( subs -> subs.add(newSub));
-
-//        List<Observer> user = eventSub.get(eventType);
-//        user.add(newSub);
+                .ifPresentOrElse(subs -> subs.add(newSub), () -> {
+                    List<Observer> listEvents = new ArrayList<>();
+                    listEvents.add(newSub);
+                    eventSub.put(eventType, listEvents);
+                });
     }
 
-    public void unsubscribe (String eventType, Observer oldSub){
-
-
+    public void unsubscribe(String eventType, Observer oldSub) {
         Optional.ofNullable(eventSub.get(eventType))
-                .ifPresent( subs -> subs.remove(oldSub));
-
-//        List<Observer> user = eventSub.get(eventType);
-//        user.remove(oldSub);
+                .ifPresent(subs -> subs.remove(oldSub));
     }
 
-    public void notifySubscriber(String eventType, String message){
-
+    public void notifySubscriber(String eventType, String message) {
         Optional.ofNullable(eventSub.get(eventType))
-                .ifPresent(subs -> subs. forEach(sub-> sub.update(message)));
+                .ifPresent(subs -> subs.forEach(sub -> sub.update(message)));
     }
 
 }
--- src/main/java/org/example/BookSpring/bookStorage/Notification/SubscriberConfig.java
+++ src/main/java/org/example/BookSpring/bookStorage/Notification/SubscriberConfig.java
@@ -2,37 +2,16 @@ package org.example.BookSpring.bookStorage.Notification;
 
 import org.example.BookSpring.bookStorage.Notification.subscribers.Subscriber;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-@Component
+@Configuration
 public class SubscriberConfig {
 
-    private final Subscriber subscriber;
-
     @Autowired
-    public SubscriberConfig(Subscriber subscriber){
-            this.subscriber = subscriber;
+    public SubscriberConfig(Subscriber subscriber, ManageSubscriber manageSubscriber) {
+
+        manageSubscriber.subscribe("book", subscriber);
+        manageSubscriber.subscribe("magazine", subscriber);
+        manageSubscriber.subscribe("letter", subscriber);
     }
-
-    public Map<String, List<Observer>> allEvents(){
-
-        Map<String, List<Observer>> eventSub = new HashMap<>();
-
-        List<Observer> observers = new ArrayList<>();
-        observers.add(subscriber);
-
-        eventSub.put("book", observers);
-        eventSub.put("magazine", observers);
-        eventSub.put("letter", observers);
-
-
-        return eventSub;
-    }
-
 }
