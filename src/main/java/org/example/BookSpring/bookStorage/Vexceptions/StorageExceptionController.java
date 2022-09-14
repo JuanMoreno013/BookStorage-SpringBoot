48c81f73242671f72cd23b41177a39c4df62e7f0
Juan Moreno <jmoreno@griddynamics.com>
2022-09-13 19:17:49 -0500

    Modify StorageExceptionController

--- src/main/java/org/example/BookSpring/bookStorage/Vexceptions/StorageExceptionController.java
+++ src/main/java/org/example/BookSpring/bookStorage/Vexceptions/StorageExceptionController.java
@@ -1,30 +1,35 @@
 package org.example.BookSpring.bookStorage.Vexceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
 @ControllerAdvice
-public class StorageExceptionController {
+public class StorageExceptionController extends ResponseEntityExceptionHandler {
 
     @ResponseBody
     @ExceptionHandler(ItemNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String itemNotFoundHandler(ItemNotFoundException exception){
-        return exception.getMessage();
+    ResponseEntity<Object> itemNotFoundHandler(ItemNotFoundException exception) {
+        return errorResponse(exception, HttpStatus.NOT_FOUND);
     }
 
 
     @ResponseBody
     @ExceptionHandler(ItemBadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    String itemBadRequestHandler( ItemBadRequestException exception){
-        return exception.getMessage();
+    ResponseEntity<Object> itemBadRequestHandler(ItemBadRequestException exception) {
+        return errorResponse(exception, HttpStatus.BAD_REQUEST);
     }
 
+    private ResponseEntity<Object> errorResponse(Exception e, HttpStatus httpStatus) {
+        return ResponseEntity.status(httpStatus).body(e.getMessage());
+    }
 }
 
 
