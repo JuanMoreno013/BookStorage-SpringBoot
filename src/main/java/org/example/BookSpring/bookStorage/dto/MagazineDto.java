c28ca9f12d8cde194881ff00725222347fcdb5bc
Juan Moreno <jmoreno@griddynamics.com>
2022-09-22 13:42:57 -0500

    Create Entity and Dto for Magazine

--- src/main/java/org/example/BookSpring/bookStorage/dto/MagazineDto.java
+++ src/main/java/org/example/BookSpring/bookStorage/dto/MagazineDto.java
@@ -1,35 +1,25 @@
-package org.example.BookSpring.bookStorage.models;
+package org.example.BookSpring.bookStorage.dto;
 
-import lombok.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.example.BookSpring.bookStorage.validators.ChainValidator;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
 import java.time.LocalDate;
 
-@Entity
-@Table(name = "Magazines")
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
-@NamedQuery(query = "select u from Magazine u", name = "query_find_all_magazines")
-public final class Magazine extends ItemOp {
-
-    @Column(name = "volume")
+public class MagazineDto extends ItemOpDto {
     private int volume;
-    @Column(name = "subject")
     private String subject;
-    @Column(name = "editorial")
     private String editorial;
 
-    @Column(name = "user_taken")
     private Integer user_taken;
 
     private static ChainValidator validateChain = new ChainValidator();
 
-    public Magazine(String title, String author, int pages, LocalDate date_write, String subject, int volume, String editorial) {
+    public MagazineDto(String title, String author, int pages, LocalDate date_write, String subject, int volume, String editorial) {
         super(title, author, pages, date_write);
         this.volume = validateChain.processValidator(volume);
         this.subject = validateChain.processValidator(subject);
