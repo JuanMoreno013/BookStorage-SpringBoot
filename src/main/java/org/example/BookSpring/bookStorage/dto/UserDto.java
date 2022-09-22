b2a23bba41dfc7722f98c6e57384ce05c3d6a43e
Juan Moreno <jmoreno@griddynamics.com>
2022-09-22 13:40:28 -0500

    Create Entity and Dto for User

--- src/main/java/org/example/BookSpring/bookStorage/dto/UserDto.java
+++ src/main/java/org/example/BookSpring/bookStorage/dto/UserDto.java
@@ -1,40 +1,25 @@
-package org.example.BookSpring.bookStorage.models;
-
+package org.example.BookSpring.bookStorage.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.BookSpring.bookStorage.validators.ChainValidator;
 
-
-import javax.persistence.*;
 import java.time.LocalDate;
 
-
-@Entity
-@Table(name = "Users")
 @Data
 @NoArgsConstructor
-@NamedQuery(query = "select u from User u", name = "query_find_all_users")
-public final class User {
+public class UserDto {
 
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(name = "user_name")
     private String userName;
-    @Column(name = "date_birth")
     private LocalDate dateBirth;
-    @Column(name = "date_take_item")
     private LocalDate dateTakeItem;
 
     private static ChainValidator validateChain = new ChainValidator();
 
-    public User(String userName, LocalDate dateBirth, LocalDate dateTakeItem) {
+    public UserDto(String userName, LocalDate dateBirth, LocalDate dateTakeItem) {
         this.userName = userName;
         this.dateBirth = validateChain.processValidator(dateBirth);
         this.dateTakeItem = validateChain.processValidator(dateTakeItem);
     }
-
-
 }
