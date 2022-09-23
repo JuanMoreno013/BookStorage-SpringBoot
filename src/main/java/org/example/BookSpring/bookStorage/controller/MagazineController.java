03dacb53a220e58d90b35976172ff00ab4afbf9a
Juan Moreno <jmoreno@griddynamics.com>
2022-09-23 11:03:22 -0500

    Modify Service and Controller of Magazine

--- src/main/java/org/example/BookSpring/bookStorage/controller/MagazineController.java
+++ src/main/java/org/example/BookSpring/bookStorage/controller/MagazineController.java
@@ -1,8 +1,10 @@
 package org.example.BookSpring.bookStorage.controller;
 
-import org.example.BookSpring.bookStorage.models.Magazine;
+import org.example.BookSpring.bookStorage.dto.MagazineDto;
+import org.example.BookSpring.bookStorage.dto.TakenItemDto;
 import org.example.BookSpring.bookStorage.service.MagazineService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,27 +19,33 @@ public class MagazineController {
     }
 
     @GetMapping("/magazines")
-    public List<Magazine> getAllMagazines() {
+    public List<MagazineDto> getAllMagazines() {
         return magazineService.getAll();
     }
 
-    @GetMapping("/magazines/{magazineId}")
-    public Magazine getMagazine(@PathVariable Integer magazineId) {
-        return magazineService.get(magazineId);
+    @GetMapping("/magazines/{magazineDtoId}")
+    public MagazineDto getMagazine(@PathVariable Integer magazineDtoId) {
+        return magazineService.get(magazineDtoId);
     }
 
     @PostMapping("/magazines")
-    public Boolean addMagazine(@RequestBody Magazine magazine) {
-        return magazineService.save(magazine);
+    @ResponseStatus(HttpStatus.CREATED)
+    public MagazineDto addMagazine(@RequestBody MagazineDto magazineDto) {
+        return magazineService.save(magazineDto);
     }
 
-    @DeleteMapping("/magazines/{magazineId}")
-    public Boolean deleteMagazine(@PathVariable Integer magazineId) {
-        return magazineService.delete(magazineId);
+    @DeleteMapping("/magazines/{magazineDtoId}")
+    public Boolean deleteMagazine(@PathVariable Integer magazineDtoId) {
+        return magazineService.delete(magazineDtoId);
     }
 
-    @PutMapping("/magazines/{magazineId}")
-    public Boolean updateMagazine(@PathVariable Integer magazineId, @RequestBody Magazine magazine) {
-       return magazineService.update(magazineId, magazine);
+    @PutMapping("/magazines/{magazineDtoId}")
+    public Boolean updateMagazine(@PathVariable Integer magazineDtoId, @RequestBody MagazineDto magazineDto) {
+       return magazineService.update(magazineDtoId, magazineDto);
+    }
+
+    @PutMapping("/magazines/{magazineDtoId}/takenBy")
+    public Boolean updateMagazineTakenByUser(@PathVariable Integer magazineDtoId,  @RequestBody TakenItemDto takenItemDto) {
+        return magazineService.updateMagazineTakenBy(magazineDtoId, takenItemDto);
     }
 }
--- src/main/java/org/example/BookSpring/bookStorage/service/MagazineService.java
+++ src/main/java/org/example/BookSpring/bookStorage/service/MagazineService.java
@@ -1,57 +1,91 @@
 package org.example.BookSpring.bookStorage.service;
 
-import org.example.BookSpring.bookStorage.models.Magazine;
+import org.example.BookSpring.bookStorage.converter.MagazineConverter;
+import org.example.BookSpring.bookStorage.dto.MagazineDto;
+import org.example.BookSpring.bookStorage.dto.TakenItemDto;
 import org.example.BookSpring.bookStorage.exceptionhandler.ItemBadRequestException;
 import org.example.BookSpring.bookStorage.exceptionhandler.ItemNotFoundException;
+import org.example.BookSpring.bookStorage.models.Magazine;
 import org.example.BookSpring.bookStorage.notification.NotificationCenter;
-import org.example.BookSpring.bookStorage.repository.MagazineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
+@Transactional
 public class MagazineService {
-    private final MagazineRepository repository;
-    private final NotificationCenter notificationCenter;
-
+    @PersistenceContext
+    private EntityManager entityManager;
     @Autowired
-    public MagazineService(MagazineRepository repository, NotificationCenter notificationCenter) {
-        this.repository = repository;
-        this.notificationCenter = notificationCenter;
+    private NotificationCenter notificationCenter;
+    @Autowired
+    private MagazineConverter converter;
+
+
+    public List<MagazineDto> getAll() {
+        Query query = entityManager.createNamedQuery("query_find_all_magazines", Magazine.class);
+        List daoMagazineList = query.getResultList();
+        return converter.listEntityToDTO(daoMagazineList);
+
     }
 
-    public List<Magazine> getAll() {
-        return repository.findAll();
+    public MagazineDto get(int magazineDtoId) {
+        return Optional.of(converter.entityToDTO(entityManager.find(Magazine.class, magazineDtoId)))
+                .orElseThrow(() -> new ItemNotFoundException(" NOT found Magazine with ID : " + magazineDtoId));
     }
 
-    public Magazine get(int magazineId) {
+    public MagazineDto save(MagazineDto magazineDto) {
+        Objects.requireNonNull(magazineDto);
 
-        return repository.get(magazineId)
-                .orElseThrow(() -> new ItemNotFoundException(" NOT found book with ID : " + magazineId));
-    }
-
-    public Boolean save(Magazine magazine) {
-        Objects.requireNonNull(magazine);
-
+        entityManager.persist(converter.dtoToEntity(magazineDto));
         notificationCenter.sendNotification("magazine", "New Magazine Added !");
-        return repository.save(magazine) == 1;
-
+        return magazineDto;
     }
 
-    public Boolean delete(int magazineId) {
-        if (magazineId < 1)
+    public Boolean delete(int magazineDtoId) {
+        if (magazineDtoId < 1)
             throw new ItemBadRequestException(" Wrong ID number! ");
 
+        Optional<Magazine> foundMagazine = Optional.of(entityManager.find(Magazine.class, magazineDtoId));
+        foundMagazine.ifPresent((magazineId -> entityManager.remove(magazineId)));
+
         notificationCenter.sendNotification("magazine", " Magazine Removed !");
-        return repository.delete(magazineId) == 1;
-
+        return true;
     }
 
-    public Boolean update(int magazineId, Magazine magazineItem) {
+    public Boolean update(int magazineDtoId, MagazineDto magazineDto) {
 
-        return repository.update(magazineId, magazineItem) == 1;
+        Optional<Magazine> foundBook = Optional.of(entityManager.find(Magazine.class, magazineDtoId));
+        foundBook.ifPresent((magazine -> {
+
+            Magazine magazineEntity = converter.dtoToEntity(magazineDto);
+            magazineEntity.setId(magazineDtoId);
+            entityManager.merge(magazineEntity);
+        }));
+        return true;
     }
+
+
+    public Boolean updateMagazineTakenBy(int magazineDtoId, TakenItemDto takenItemDto) {
+
+        Optional<Magazine> foundMagazine = Optional.of(entityManager.find(Magazine.class, magazineDtoId));
+        foundMagazine.ifPresent((magazine -> {
+
+            Magazine magazineEntity = foundMagazine.get();
+            magazineEntity.setUser_taken(takenItemDto.getUser_taken());
+            magazineEntity.setId(magazineDtoId);
+
+            entityManager.merge(magazineEntity);
+        }));
+        return true;
+    }
+
 
 }
